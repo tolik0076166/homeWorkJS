944377faaf8cec04d8e6edcfa98a193042437700
tolik00761666 <tolik007616@gmail.com>
2023-06-27 22:06:19 +0300

    Viewed image

---
+++ Viewed image/index.js
@@ -0,0 +1,9 @@
+let images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"];
+
+let randomIndex = Math.floor(Math.random() * images.length);
+
+let randomImage = images[randomIndex];
+
+let imageElement = document.getElementById("randomImage");
+
+imageElement.src = "images/" + randomImage;
