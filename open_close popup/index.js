f70defe4fed3f045abe12ae4f55b9f8e6f2e1710
tolik00761666 <tolik007616@gmail.com>
2023-07-18 23:26:56 +0300

    Popup

---
+++ open_close popup/index.js
@@ -0,0 +1,12 @@
+function openPopup() {
+    popup = window.open(
+    'https://github.com/', 
+    'popupWindow', 
+    'width=600', + 
+    'height=400'
+    );
+}
+
+function closePopup() {
+    popup.close();
+}
